2d91515d0607ee0490ca90bfe8b0614104295162
git-jessryll-workspace <jessryll.workspace@gmail.com>
2023-09-21 20:37:58 +0800

    tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css'
 
 function App() {
   
   return (
-    <div>
-      <div>
-        
+    <div className="flex justify-center">
+      <div className="w-1/2 mt-10 border border-gray-300">
+        <div>
+          <input className="border border-gray-600 p-1"/>
+        </div>
       </div>
     </div>
   )
